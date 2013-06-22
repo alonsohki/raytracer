227076d2181294cf5a3be6e955b7bdaa7408f78d
Alberto Alonso <rydencillo@gmail.com>
2013-06-22 19:51:51 +0300

    Model loading.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include "ply_reader.h"
+#include "profiler.h"
 #include "vector.h"
 
 int main(int argc, const char* argv[], const char* envp[])
 {
-	vec3f v ( 1.0f, 2.0f, 3.0f );
-
-	v *= 2;
-
-	std::cout << (std::string)v << std::endl;
-	std::cout << length(v) << std::endl;
+	Profiler profiler;
+	std::auto_ptr<PLY_Model> model;
+	
+	const char* models[] = { "models/happy_vrip.ply",
+		                     "models/happy_vrip_res2.ply",
+						     "models/happy_vrip_res3.ply",
+						     "models/happy_vrip_res4.ply" };
+	for ( auto model : models )
+	{
+		std::cout << "Loading model " << model << std::flush;
+		profiler.start();
+		Read_PLY_Model(model);
+		std::cout << " took: " << (profiler.stop() / 1000.0) << " seconds " << std::endl;
+	}
 
 	system("pause");
 	return EXIT_SUCCESS;
---
+++ src/profiler.h
@@ -0,0 +1,60 @@
+#ifdef WIN32
+
+#include <windows.h>
+
+class Profiler
+{
+public:
+	Profiler ()
+	: mRunning(false)
+	{
+		LARGE_INTEGER li;
+		if ( !QueryPerformanceFrequency(&li) )
+			throw "QueryPerformanceFrequency failed";
+
+		mFreq = double(li.QuadPart) / 1000.0;
+	}
+
+	void start ()
+	{
+		LARGE_INTEGER li;
+		mRunning = true;
+
+		QueryPerformanceCounter(&li);
+		mStart = li.QuadPart;
+	}
+
+	double stop ()
+	{
+		LARGE_INTEGER li;
+		QueryPerformanceCounter(&li);
+		mEnd = li.QuadPart;
+
+		mRunning = false;
+		return query ();
+	}
+
+	double query () const
+	{
+		if ( mRunning )
+		{
+			LARGE_INTEGER li;
+			QueryPerformanceCounter(&li);
+			return double(li.QuadPart - mStart)/mFreq; 
+		}
+		else
+		{
+			return (mEnd - mStart)/mFreq;
+		}
+	}
+
+	bool isRunning () const { return mRunning; }
+
+private:
+	bool			mRunning;
+	double			mFreq;
+	__int64			mStart;
+	__int64			mEnd;
+};
+
+#endif
