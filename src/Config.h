753cb19a4aaa729d79c6687533aaf66ffa39f0b7
Alberto Alonso <rydencillo@gmail.com>
2013-07-06 14:16:38 +0300

    Added Config.h and Face.h

---
+++ src/Config.h
@@ -0,0 +1,10 @@
+//
+// FILE:        Config.h
+// LICENSE:     The MIT license
+// PURPOUSE:    App configuration.
+// AUTHORS:     Alberto Alonso <rydencillo@gmail.com>
+//
+
+#pragma once
+
+//#define USE_KDTREES
---
+++ src/Face.h
@@ -0,0 +1,16 @@
+//
+// FILE:        Face.h
+// LICENSE:     The MIT license
+// PURPOUSE:    Structure representing a triangle face.
+// AUTHORS:     Alberto Alonso <rydencillo@gmail.com>
+//
+
+#pragma once
+
+class Face
+{
+public:
+    int v1;
+    int v2;
+    int v3;
+};
